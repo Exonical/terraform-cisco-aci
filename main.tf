cf07bf19e979b4e7437aca3268ffaea6b63e7c8b
Bryce A <bryce.m.anglin@protonmail.com>
2024-04-13 22:57:37 -0400

    add providers

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-# Configure the Cisco ACI Provider
-terraform {
-  required_providers {
-    aci = {
-      source  = "CiscoDevNet/aci"
-      version = "~> 2.14.0"  # Specify the version you want to use
-    }
-  }
-}
-
-provider "aci" {
-  url      = "https://apic.example.com"  # APIC URL
-  username = "admin"                    # APIC Username
-  password = "password"                 # APIC Password
-  insecure = true                       # Skip SSL certificate verification
-}
-
-
 provider "aci" {
   url      = "https://apic-url"
   username = "admin"
@@ -28,4 +10,8 @@ module "aci_network" {
   tenant_dn         = aci_tenant.example_tenant.id
   app_profile_dn    = aci_app_profile.example_app_profile.id
   vrfs              = var.vrfs
+
+  providers = {
+    aci = aci.ciscodevnet
+  }
 }
---
+++ modules/aci_network/versions.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    aci = {
+      source  = "CiscoDevNet/aci"
+      version = "~> 2.14.0"  # Specify the version you want to use
+    }
+  }
+}
